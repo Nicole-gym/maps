e715eec9a7f25e049974f9ce11dedc92e2c677b9
Nicole-gym <yg2529@columbia.edu>
2021-04-06 16:42:44 -0700

    add company class

--- src/User.ts
+++ src/User.ts
@@ -1,9 +1,19 @@
-class User {
+import faker from 'faker';
+
+export class User {
   name: string;
   location: {
     lag: number;
     lng: number;
   };
+
+  constructor() {
+    this.name = faker.name.findName();
+    this.location = {
+      lag : parseFloat(faker.address.latitude()),
+      lng : parseFloat(faker.address.longitude())
+    };
+  }
 }
 
 //this is for testing password
